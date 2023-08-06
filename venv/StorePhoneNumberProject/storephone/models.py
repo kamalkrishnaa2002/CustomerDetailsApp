dac84bcbb102f34bb50f0304868f81ceca743c43
kamalkrishnaa2002 <135103879+kamalkrishnaa2002@users.noreply.github.com>
2023-08-06 22:37:21 +0530

    updated button hides and dates added

--- venv/StorePhoneNumberProject/storephone/models.py
+++ venv/StorePhoneNumberProject/storephone/models.py
@@ -1,6 +1,3 @@
-# Create your models here.
-# storephone/models.py
-
 from django.db import models
 
 class Category(models.Model):
@@ -9,13 +6,13 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-from django.db import models
-
 class Store(models.Model):
     name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=20)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     button_clicked = models.BooleanField(default=False)  # New field
+    registered_date = models.DateTimeField(auto_now_add=True)  # New field
+    clicked_at_date = models.DateTimeField(null=True, blank=True)  # New field
 
     def __str__(self):
         return self.name
--- venv/StorePhoneNumberProject/storephone/views.py
+++ venv/StorePhoneNumberProject/storephone/views.py
@@ -5,28 +5,23 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from .models import Store
-
-
+from django.utils import timezone
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 
 def store_form(request):
-
     if request.method == 'POST':
         form = StoreForm(request.POST)
         if form.is_valid():
-            form.save()
+            store = form.save(commit=False)
+            store.registered_date = timezone.now()  # Import timezone if not imported
+            store.save()
             return redirect('store_list')
     else:
         form = StoreForm()
-    
+
     context = {'form': form}
     return render(request, 'storephone/store_form.html', context)
-
-
-
-from django.shortcuts import render
-from .models import Store, Category
-
-
 
 
 def store_list_view(request):
@@ -41,6 +36,8 @@ def store_list_view(request):
 
     if selected_status == "clicked":
         stores = stores.filter(button_clicked=True)
+        # Update clicked_at_date for clicked stores
+        Store.objects.filter(pk__in=stores).update(clicked_at_date=timezone.now())
     elif selected_status == "unclicked":
         stores = stores.filter(button_clicked=False)
 
@@ -54,8 +51,6 @@ def store_list_view(request):
     return render(request, 'storephone/store_list.html', context)
 
 
-
-
 def create_category(request):
     if request.method == 'POST':
         form = CategoryForm(request.POST)
@@ -67,12 +62,19 @@ def create_category(request):
     return render(request, 'storephone/create_category.html', {'form': form})
 
 
+
 def update_button_clicked(request, store_id):
-    store = get_object_or_404(Store, pk=store_id)
     if request.method == 'POST':
-        store.button_clicked = True
-        store.save()
-    return redirect('store_list')  # Replace 'store_list' with the name of your store list view
+        store = get_object_or_404(Store, pk=store_id)
+        if not store.button_clicked:
+            store.button_clicked = True
+            store.clicked_at_date = timezone.now()
+            store.save()
+            return JsonResponse({'status': 'success'})
+        else:
+            return JsonResponse({'status': 'already_clicked'})
+
+    return JsonResponse({'status': 'invalid_method'})
 
 
 def send_whatsapp(request, store_id):
